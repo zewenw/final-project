15bb54c6bd4c00bfee67fb260fd7b892e913e88c
zewenw <wangzewen8527@163.com>
2023-06-15 15:18:59 +0100

    initialization

---
+++ gateway/src/test/java/com/wang/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wang.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
