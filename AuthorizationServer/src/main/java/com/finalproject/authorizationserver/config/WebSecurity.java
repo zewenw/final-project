55f14d45b3c6cd5c88bcc467ec78fda0c42ae08f
zewenw <wangzewen8527@163.com>
2023-06-17 18:27:18 +0100

    oauth 2.0 authorization server

---
+++ AuthorizationServer/src/main/java/com/finalproject/authorizationserver/config/WebSecurity.java
@@ -0,0 +1,38 @@
+package com.finalproject.authorizationserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurity {
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        var userDetails = User.withUsername("user").password("{noop}123").authorities("read").build();
+        var manager = new InMemoryUserDetailsManager();
+        manager.createUser(userDetails);
+        return manager;
+    }
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
+            throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated()
+                )
+                // Form login handles the redirect to the login page from the
+                // authorization server filter chain
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
