17dfc66bd72a267de105c55a45d544cc715b2ff0
zewenw <wangzewen8527@163.com>
2023-06-26 16:20:18 +0100

    Config server and client- can be start successfully

---
+++ TestModule/src/main/java/com/finalproject/testmodule/TestModuleApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.testmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestModuleApplication.class, args);
+    }
+
+}
---
+++ TestModule/src/test/java/com/finalproject/testmodule/TestModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.testmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
