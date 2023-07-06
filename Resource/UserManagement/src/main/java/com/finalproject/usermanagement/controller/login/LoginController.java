7b808366f5fce7cbdf0f3df3da00e883d2d9057f
zewenw <wangzewen8527@163.com>
2023-07-06 16:52:07 +0100

    complete role base control config gateway as a oauth client and authorization server as a customized auth server config user management server as a resource server

---
+++ Resource/UserManagement/src/main/java/com/finalproject/usermanagement/controller/login/LoginController.java
@@ -0,0 +1,14 @@
+package com.finalproject.usermanagement.controller.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/loginPage")
+    public String loginPage(){
+        return "login-page";
+    }
+
+}
