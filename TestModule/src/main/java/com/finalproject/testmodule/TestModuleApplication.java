4ff1b65b44c131b009f432b3f7562ac679b98b3a
zewenw <wangzewen8527@163.com>
2023-07-08 09:56:17 +0100

    oauth interaction diagram

--- TestModule/src/main/java/com/finalproject/testmodule/TestModuleApplication.java
+++ TestModule/src/main/java/com/finalproject/testmodule/TestModuleApplication.java
@@ -2,10 +2,9 @@ package com.finalproject.testmodule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class TestModuleApplication {
 
     public static void main(String[] args) {
