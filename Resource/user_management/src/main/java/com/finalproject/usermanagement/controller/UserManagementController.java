28aa5d85edb53926b8b5ea517e24af5fc31a3a98
zewenw <wangzewen8527@163.com>
2023-07-09 12:17:55 +0100

    add some config

--- Resource/user_management/src/main/java/com/finalproject/usermanagement/controller/UserManagementController.java
+++ Resource/user_management/src/main/java/com/finalproject/usermanagement/controller/UserManagementController.java
@@ -1,5 +1,6 @@
 package com.finalproject.usermanagement.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserManagementController {
 
+    @Value("${custom.id}")
+    private String id;
+
     @GetMapping("/sayHello")
     public String sayHello(){
-        return "user management....";
+        return "user management....id is " + id;
     }
 
 }
