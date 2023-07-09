55679ec32181e271e22f7caf98e9d879dde4b89d
zewenw <wangzewen8527@163.com>
2023-07-09 17:20:06 +0100

    change eureka server name

--- eureka_server/src/main/java/com/finalproject/eurekaserver/EurekaServerApplication.java
+++ eureka_server/src/main/java/com/finalproject/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.finalproject.discoveryservice;
+package com.finalproject.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class DiscoveryServiceApplication {
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiscoveryServiceApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 
 }
