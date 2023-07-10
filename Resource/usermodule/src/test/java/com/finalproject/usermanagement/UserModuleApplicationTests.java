539fbcc6f7870f75eec08171c32d6823e1cb54a5
zewenw <wangzewen8527@163.com>
2023-07-10 15:47:19 +0100

    change user module name

---
+++ Resource/usermodule/src/test/java/com/finalproject/usermanagement/UserModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
