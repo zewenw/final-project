6ea3cf7e9cfcbbdf4e515bec780c66a931ac448e
zewenw <wangzewen8527@163.com>
2023-07-11 17:41:29 +0100

    circuit breaker pattern

--- Resource/photo/src/main/java/com/finalproject/photo/controller/PhotoController.java
+++ Resource/photo/src/main/java/com/finalproject/photo/controller/PhotoController.java
@@ -2,6 +2,7 @@ package com.finalproject.photo.controller;
 
 
 import com.finalproject.photo.feign.client.UserFeignClient;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +16,19 @@ public class PhotoController {
     private UserFeignClient userFeignClient;
 
     @GetMapping("/sayHello")
+    @CircuitBreaker(name = "detailsForUserApp", fallbackMethod = "defaultFallbackMethod")
     public String sayHello(){
         String id = userFeignClient.getId();
-        return "user management....id is " + id;
+        return "photo component and the id is " + id;
+    }
+
+    /**
+     * fallback method should have same signature with original method and add throwable as a mandatory parameter
+     * then the fallback method shouldn't invoke corresponding service in order to avoid failure
+     * @param t
+     * @return
+     */
+    public String defaultFallbackMethod(Throwable t){
+        return "photo component and this is the default fallback method";
     }
 }
