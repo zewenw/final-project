dfec3bd26bd0e2a3c3aebd0fd717bd303855921f
zewenw <wangzewen8527@163.com>
2023-07-11 18:51:57 +0100

    docker-compose.yml

--- Resource/photo/src/main/java/com/finalproject/photo/controller/resillience/RateLimiterPatternController.java
+++ Resource/photo/src/main/java/com/finalproject/photo/controller/resillience/RateLimiterPatternController.java
@@ -1,7 +1,7 @@
 package com.finalproject.photo.controller.resillience;
 
 
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +12,7 @@ public class RateLimiterPatternController {
 
 
     @GetMapping("/ratelimiter")
-    @Retry(name = "rateLimiterForUserApp", fallbackMethod = "rateLimiterFallbackMethod")
+    @RateLimiter(name = "defaultRateLimiter", fallbackMethod = "rateLimiterFallbackMethod")
     public String sayHello(){
         return "photo component";
     }
