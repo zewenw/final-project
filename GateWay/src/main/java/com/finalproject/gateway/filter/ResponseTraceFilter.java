a0413a023504545a508e360de2f9c63b1a71759f
zewenw <wangzewen8527@163.com>
2023-07-13 14:58:18 +0100

    global trace id

---
+++ GateWay/src/main/java/com/finalproject/gateway/filter/ResponseTraceFilter.java
@@ -0,0 +1,26 @@
+package com.finalproject.gateway.filter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class ResponseTraceFilter {
+
+    @Autowired
+    FilterUtility filterUtility;
+
+    @Bean
+    public GlobalFilter postGlobalFilter() {
+        return (exchange, chain) -> {
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
+                String correlationId = filterUtility.getCorrelationId(requestHeaders);
+                exchange.getResponse().getHeaders().add(filterUtility.CORRELATION_ID, correlationId);
+            }));
+        };
+    }
+}
