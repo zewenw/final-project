8747f4c1a478f3be7711ffe273c4043d58cbe184
zewenw <wangzewen8527@163.com>
2023-07-29 09:56:37 +0100

    webflux initializaiton

---
+++ webflux/src/test/java/com/finalproject/webflux/WebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
