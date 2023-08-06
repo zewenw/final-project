793a0b66b8bb8ef6b4c215ebf45e8557406c5fae
zewenw <wangzewen8527@163.com>
2023-08-06 12:20:40 +0100

    sort out file

---
+++ user/src/main/java/com/finalproject/user/controller/DemoController.java
@@ -0,0 +1,54 @@
+package com.finalproject.user.controller;
+
+import com.finalproject.foundation.utils.UserUtil;
+import com.finalproject.user.service.UserService;
+import io.micrometer.core.annotation.Timed;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+@Slf4j
+public class DemoController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/sayHello")
+    @Timed(value = "sayHello", description = "this is the sayHello method")
+    public String sayHello() {
+        log.info("user component, say hello, method start");
+        String username = UserUtil.getCurrentUsername();
+        log.info("=============current user is {}", username);
+        String id = userService.getId();
+        log.info("user component, say hello, method end");
+        return "user management....id is " + id;
+    }
+
+    @PostMapping("/id")
+    public String getId() {
+        return userService.getId();
+    }
+
+    @GetMapping("/status/check")
+//    @Scope("SCOPE_profile")
+    public String status() {
+        return "Working...";
+    }
+
+    //    @PreAuthorize("hasAuthority('ROLE_USER')")
+//    @Secured("ROLE_USER")
+    @DeleteMapping(path = "/{id}")
+    public String deleteUser(@PathVariable String id) {
+        return "Deleted user with id " + id;
+    }
+
+
+    //    @PostAuthorize("returnObject.userId == 'aaa'")
+//    @GetMapping(path = "/{id}")
+//    public UserRest getUser(@PathVariable String id) {
+//        System.out.println("has been executed");
+//        return new UserRest("Sergey", "Kargopolov", id);
+//    }
+}
--- user/src/main/java/com/finalproject/user/controller/UserController.java
+++ user/src/main/java/com/finalproject/user/controller/UserController.java
@@ -1,13 +1,12 @@
 package com.finalproject.user.controller;
 
 
-import com.finalproject.foundation.utils.UserUtil;
 import com.finalproject.user.service.UserService;
-import io.micrometer.core.annotation.Timed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
@@ -19,41 +18,6 @@ public class UserController {
     private UserService userService;
 
 
-    @GetMapping("/sayHello")
-    @Timed(value = "sayHello", description = "this is the sayHello method")
-    public String sayHello() {
-        logger.info("user component, say hello, method start");
-        String username = UserUtil.getCurrentUsername();
-        logger.info("=============current user is {}", username);
-        String id = userService.getId();
-        logger.info("user component, say hello, method end");
-        return "user management....id is " + id;
-    }
 
-    @PostMapping("/id")
-    public String getId() {
-        return userService.getId();
-    }
-
-    @GetMapping("/status/check")
-//    @Scope("SCOPE_profile")
-    public String status() {
-        return "Working...";
-    }
-
-    //    @PreAuthorize("hasAuthority('ROLE_USER')")
-//    @Secured("ROLE_USER")
-    @DeleteMapping(path = "/{id}")
-    public String deleteUser(@PathVariable String id) {
-        return "Deleted user with id " + id;
-    }
-
-
-    //    @PostAuthorize("returnObject.userId == 'aaa'")
-//    @GetMapping(path = "/{id}")
-//    public UserRest getUser(@PathVariable String id) {
-//        System.out.println("has been executed");
-//        return new UserRest("Sergey", "Kargopolov", id);
-//    }
 
 }
