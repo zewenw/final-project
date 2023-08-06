86866f9c6ee0da5a91090eeff2c80b7a916a53a4
zewenw <wangzewen8527@163.com>
2023-08-06 12:20:58 +0100

    sort out file

--- user/src/main/java/com/finalproject/user/controller/DemoController.java
+++ user/src/main/java/com/finalproject/user/controller/DemoController.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/demo")
 @Slf4j
 public class DemoController {
 
