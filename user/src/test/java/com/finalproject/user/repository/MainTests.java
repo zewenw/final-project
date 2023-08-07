c8f198c910cca11dc94d7da0de7610819d8eed1c
zewenw <wangzewen8527@163.com>
2023-08-07 10:37:39 +0100

    some changes

---
+++ user/src/test/java/com/finalproject/user/repository/MainTests.java
@@ -0,0 +1,12 @@
+package com.finalproject.user.repository;
+
+public class MainTests {
+
+    public static void main(String[] args) {
+        int i = 0;
+        Integer j = new Integer(0);
+        System.out.println(i == j);
+        System.out.println(i);
+        System.out.println(j);
+    }
+}
